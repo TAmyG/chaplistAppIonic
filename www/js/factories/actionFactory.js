9d4c10ef01660a7fd6fea32d329966dbb2ed1ca3
kuea <k3n3th@gmail.com>
2016-02-04 14:05:29 -0600

    cambio en el uso de localstorage, y creación de métodos para obtener y verificar tokens

--- www/js/factories/actionFactory.js
+++ www/js/factories/actionFactory.js
@@ -1,96 +1,108 @@
 angular.module('actionFactory', [])
-//*************************************************************************************************************************
-.factory('factory', function($http, $ionicPopup, $cordovaDevice, $localStorage){
-    var comun  = {};
-    comun.token = null;
-    comun.supermarkets = [];
-    var packagaName = 'com.ionicframework.betasocial427641';
-    var secretKey = '9be5be62ebcd7c1139ea14cc0a0f8e9d6df96053b95a2677dc4a4e4bad4392d5';
-    //var secretKey = '8e9d6df96053b95a2677dc4a4e4bad4392d5';
+    //*************************************************************************************************************************
+    .factory('factory', function ($http, $ionicPopup, $cordovaDevice, $localStorage) {
+        var comun = {};
+        var packagaName = 'com.ionicframework.betasocial427641';
+        var secretKey = 'b3445460e0b140be4e1105eb8836b16fdcf88f0daa03ac231d14635515a49bbd';
+        //var secretKey = '8e9d6df96053b95a2677dc4a4e4bad4392d5';
 
-    /*
-        Función para verificar el token al primer inicio de la aplicación
-    */
-    comun.tokenVerified = function () {
-        var body = {};
-        if(comun.getToken())
-            return;
-        //de no existir un token se procede a solicitar uno a la API
-        body.packageName = packagaName;
-        body.secretKey = secretKey;
-        body.uuid = 'abcdefghijokl1234567'//$cordovaDevice.getUUID();
+        /*
+            Función para verificar el token al primer inicio de la aplicación
+        */
+        comun.tokenVerified = function () {
+                var body = {};
+                var tokenAux = {};
+               /* if (comun.existsTokenAPI())
+                    return;*/
+                //de no existir un token se procede a solicitar uno a la API
+                body.packageName = packagaName;
+                body.secretKey = secretKey;
+                body.uuid = 'abcdefghijokl1234567' //$cordovaDevice.getUUID();
 
-        return $http.post('http://192.168.0.14:8080/api/Chap/tokenPetition', body)
-            .then(function (res) {
-                if(res.status = 200){
-                    comun.token = res.data.replace('"','').replace('"','');
-                    console.log(comun.token);
-                    $localStorage.tokenApp = comunt.token;
-                    //localStorageService.set('tokenApp', comun.token);
-                    return comun.token;
-                }else{
-                    return res.data.error;
-                }
-            }, function (err) {
-                return err;
-            });
-    }
-
-
-    comun.getSupermarkets = function(){
-        var result = {};
-        return $http.get('http://192.168.0.14:8080/api/Chap/Supermarkets/'+comun.getToken())
-            .then(function (res) {
-                if(res.status =200){
-                    result = transformToJson(res.data);
-                    compareToken(result.token);
-                    comun.supermarkets = result.supermarkets;
-                    return comun.supermarkets
-                }else
-                    return res.data.error;
-            }, function (err) {
-                return err;
-            });
-    }
-
-    /*
-        Función para obtener el token actual en caso de existir sino, retorna null
-    */
-    comun.getToken = function(){
-        if(!comun.token){
-            //comun.token = localStorageService.get('tokenApp');
-            comun.token =  $localStorage.tokenApp;
-            return comun.token;
-        }else
-            return comun.token;
-    }
-
-    //*************************************************************************************************
-    /*
-        Función para comparar el token actual y reemplazarlo en caso de que halla vencido
-    */
-    function compareToken(newToken){
-        if(comun.token != newToken){
-            console.log('Es distinto', comun.token, newToken);
-            setToken(newToken);
+                return $http.post('http://localhost:8080/api/Chap/tokenPetition', body)
+                    .then(function (res) {
+                        tokenAux = res.data.replace('"', '').replace('"', '');
+                        if (res.status = 200 && tokenAux != 'null') {
+                            $localStorage.tokenAPI = tokenAux;
+                            getSupermarketsAPI();//obtiene todos los supermercados actuales
+                            return tokenAux;
+                        } else {
+                            alert('Las credenciales de la app no existen en la API');
+                            return res;
+                        }
+                    }, function (err) {
+                        return err;
+                    });
+            }
+        /*
+            Función que obtiene los supermercados desde la API
+        */
+        comun.getSupermarkets = function () {
+            return $localStorage.supermarkets;
+        }
+        /*
+            Función que verifica si existe un token válido
+        */
+        comun.existsTokenAPI = function () {
+            if ($localStorage.hasOwnProperty("tokenAPI") === true)
+                return true;
+            else
+                return false;
         }
 
-    }
+        //*************************************************************************************************
 
-    /*
-        Función para setear un token
-    */
-    function setToken(newToken){
-        comun.token = newToken;
-        $localStorage.tokenApp = newToken;
-        //localStorageService.set('tokenApp', newToken);
-    }
+        /*
+            Función para obtener los supermerdados del almacenamiento local
+        */
+        function getSupermarketsAPI() {
+            var result = {};
+            if(!comun.existsTokenAPI()){
+                alert('Esta app no tiene un token válido para el uso de la API');
+                return;
+            }
+            return $http.get('http://localhost:8080/api/Chap/Supermarkets/' + getTokenAPI())
+                .then(function (res) {
+                    if (res.status = 200) {
+                        result = transformToJson(res.data);
+                        compareToken(result.token);
+                        $localStorage.supermarkets = result.supermarkets;
+                        return result.supermarkets;
+                    } else
+                        return res.data.error;
+                }, function (err) {
+                    alert(getTokenAPI());
+                    return err;
+                });
 
-    function transformToJson(data){
-        return JSON.parse(data);
-    }
+        }
+        /*
+            Función para obtener el token actual en caso de existir sino, retorna null
+        */
+        function getTokenAPI() {
+            return $localStorage.tokenAPI;
+        }
+        /*
+            Función para comparar el token actual y reemplazarlo en caso de que halla vencido
+        */
+        function compareToken(newToken) {
+            if (getTokenAPI() != newToken) {
+                console.log('Es distinto', comun.token, newToken);
+                setToken(newToken);
+            }
+        }
 
-    return comun;
+        /*
+            Función para setear un token
+        */
+        function setToken(newToken) {
+            $localStorage.tokenAPI = newToken;
+        }
 
-})
+        function transformToJson(data) {
+            return JSON.parse(data);
+        }
 
+        return comun;
+
+    })
